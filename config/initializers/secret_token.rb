7d4b58370e2d256e8900b5149072ce4ea3d55468
vicdood <vplascencia@chicagotechacademy.org>
2013-12-13 14:41:39 -0600

    Revert "initial commit"
    This reverts commit adfe0935192e358fff3af8492d987dd630812968.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '0c16c2f160666254c4c171f382a4239570f782a9e319ae0f416a139235b224139f660b48bdc8ef5d263e2d707b9d0c45a02299c4984f74c8780281b677d43154'
